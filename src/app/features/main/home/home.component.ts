2be402603bd461669778dded515a537cc2f6b6ce
rohitchoudhary99 <rohit.choudhary@softradix.in>
2025-02-26 17:47:26 +0530

    counter code added

--- src/app/features/main/home/home.component.ts
+++ src/app/features/main/home/home.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  startDate = new Date('2025-02-25');
+  endDate = new Date('2025-11-01');
+  daysLeft: number = 0;
 
+  ngOnInit() {
+    this.calculateDaysLeft();
+  }
+
+  calculateDaysLeft() {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); // Ensure time is set to start of the day
+    const timeDiff = (this.endDate.getTime() - today.getTime());
+    this.daysLeft = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  }
 }
